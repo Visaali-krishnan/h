50ccd35dea838de64b155eb238be7be96ed5b5c1
Visaali-krishnan <kmkvisaali@gmail.com>
2016-06-06 11:25:12 +0530

    Create ty.c

---
+++ ty.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+     int num;
+     scanf("%d",&num);
+     if(num>0)
+     {
+     	printf("The number is positive");
+     }
+     else if(num==0)
+     {
+     	printf("Th number is zero");
+     }
+     else
+     {
+     	printf("The number is negative");
+     }
+	return 0;
+}
